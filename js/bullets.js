c76e119cecdbb563f18bf5567a1e6bf88a336e16
QurTu <martis55@inbox.lt>
2020-04-19 20:31:03 +0300

    bullet and sides collision works

--- js/bullets.js
+++ js/bullets.js
@@ -4,6 +4,7 @@ export default class Bullet {
         this.buletsize = 8 ;
         this.dx = speedX;
         this.dy = speedY;
+        this.DeadorAlive = 1;
         this.bulletX = shooterX;
         this.bulletY = shootery;
         this.bulletXNew = null;
@@ -41,15 +42,50 @@ export default class Bullet {
         }
 
     }
+
+    // player1andBulletsCollision() {
+    //     for(let i = 0 ; i < this.player.bulletArr.length; i++ ) {
+    //         console.log(this.player.bulletArr);
+    //             console.log('veikiu, bet neradau')
+    //         if (this.player.Player1x < this.player.bulletArr[i].bulletX + this.buletsize &&
+    //             this.player.Player1x + 48 > this.player.bulletArr[i].bulletX &&
+    //             this.player.Player1y < this.player.bulletArr[i].bulletY + this.buletsize &&
+    //             this.player.Player1xy + 48 > this.player.bulletArr[i].bulletY) {
+    //                 console.log('radau');
+    //                 this.player.bulletArr[i].DeadorAlive = 0;
+    //                 this.player.ctx.clearRect(this.bulletX, this.bulletY, this.buletsize, this.buletsize );
+    //                 this.player.bulletArr.splice(this.player.bulletArr[i]  , 1);
+    //          }
+    //     }
+    //   }
+    
+      bulletsCollWithMap() {
+        for(let i = 0 ; i < this.player.bulletArr.length; i++ )  {
+            console.log(this.player.bulletArr);
+            if (this.player.bulletArr[i].bulletX < 0 + this.buletsize || this.player.bulletArr[i].bulletX > 816 - this.buletsize  ||
+                this.player.bulletArr[i].bulletY < 0 + this.buletsize || this.player.bulletArr[i].bulletY > 624  - this.buletsize  ) {
+                    console.log('radau');
+                    this.player.bulletArr[i].DeadorAlive = 0;
+                    this.player.ctx.clearRect(this.bulletX, this.bulletY, this.buletsize, this.buletsize );
+                    this.player.bulletArr.splice(this.player.bulletArr[i]  , 1); 
+                }
+
+            }
+      }
     
 
  updateReq() {
+     if( this.DeadorAlive === 1) {
      requestAnimationFrame((e) => this.updateReq());
      this.update();
+    // this.player1andBulletsCollision();
+     this.bulletsCollWithMap();
+     }
+
  }  
 
  update() {
-    console.log("update");
+    
     this.bulletXNew += this.dx ;
     this.bulletYNew += this.dy ;
     this.updateReDraw();
