0cc48830db247188c3ddd00f1258ad3a4f784e54
QurTu <martis55@inbox.lt>
2020-04-23 02:02:46 +0300

    some fixes

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ import AllGame from './AllGame.js';
 
 
  const gameDOM = document.querySelector('.game');
+
  gameStart();
 
+
+ 
  
 function gameStart() {
     let  HTML = ` <div class="start"> <div class='start-menu'>
@@ -15,32 +18,26 @@ function gameStart() {
 </div> ` ;
    gameDOM.insertAdjacentHTML("afterbegin" , HTML) ;
     document.querySelector('.one-player').onclick = () => {
-        gameDOM.innerHTML = '';
+        while (gameDOM.firstChild) {
+            gameDOM.removeChild(gameDOM.lastChild);
+          }
         const game = new AllGame(1);  
         update(game);   
       }
     document.querySelector('.two-players').addEventListener('click', ()  =>{
-      gameDOM.innerHTML = '';
+        while (gameDOM.firstChild) {
+            gameDOM.removeChild(gameDOM.lastChild);
+          }
       const game = new AllGame(2);   
       update(game);  
 }) 
-
-
 }
-function restartGame() {
-    console.log('clicked');
-    gameDOM.innerHTML = '';
-    const game = new AllGame(1);  
-    update(game);   
-  }
-
-
 
 
 function update(game) {
     requestAnimationFrame((e) => update(game));
     if(game.alive === 0) {
-        location.reload();
+        gameStart();
     }
 
 }
