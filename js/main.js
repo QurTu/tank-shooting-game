b6fec9ff630cc632c57a30659ea0bab93f547910
QurTu <martis55@inbox.lt>
2020-04-23 01:27:30 +0300

    refresh page solution

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ import AllGame from './AllGame.js';
  
 function gameStart() {
     let  HTML = ` <div class="start"> <div class='start-menu'>
+
     <div class=" btn one-player"> START GAME : ONE PLAYER</div>
     <div class=" btn two-players">START GAME : TWO PLAYER</div>
     <div class=" btn create-lvl">CREATE LEVEL (Commint soon)</div>
@@ -24,13 +25,22 @@ function gameStart() {
       update(game);  
 }) 
 
+
 }
+function restartGame() {
+    console.log('clicked');
+    gameDOM.innerHTML = '';
+    const game = new AllGame(1);  
+    update(game);   
+  }
+
+
 
 
 function update(game) {
     requestAnimationFrame((e) => update(game));
     if(game.alive === 0) {
-        gameStart()
+        location.reload();
     }
 
 }
